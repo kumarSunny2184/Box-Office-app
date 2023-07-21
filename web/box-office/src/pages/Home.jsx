13bfe3938f3a84d150fa72e06b3aa7744cc7fd52
sunny kumar <kumarsunnysid343@gmail.com>
2023-07-21 11:03:15 +0530

    added navigation and shared layout for home and starred page

--- web/box-office/src/pages/Home.jsx
+++ web/box-office/src/pages/Home.jsx
@@ -1,11 +1,5 @@
-import { Link } from 'react-router-dom';
-
 const Home = () => {
-  return (
-    <div>
-      <Link to="/starred">Go to starred page</Link>
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default Home;
