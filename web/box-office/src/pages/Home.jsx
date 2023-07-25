7d3968a7051d80c49115f2511f0d681715b6c6d7
sunny kumar <kumarsunnysid343@gmail.com>
2023-07-25 09:50:05 +0530

    added showcard and actorcard and displayed both

--- web/box-office/src/pages/Home.jsx
+++ web/box-office/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import SearchForm from '../components/SearchForm';
 import { searchForShows, searchForPeople } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
+import ActorGrid from '../components/actors/ActorGrid';
 
 const Home = () => {
   const [apiData, setApiData] = useState(null);
@@ -27,12 +29,16 @@ const Home = () => {
       return <div>Error Occured !! {apiDataError.message}</div>;
     }
 
+    if (apiData?.length === 0) {
+      return <div>NO Results !!</div>;
+    }
+
     if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorGrid actors={apiData} />
+      );
     }
     return null;
   };
