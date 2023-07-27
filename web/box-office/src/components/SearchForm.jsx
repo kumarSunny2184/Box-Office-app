419cb6ab67db00938cbeec0aaf968bcd07712c3c
sunny kumar <kumarsunnysid343@gmail.com>
2023-07-27 16:23:19 +0530

    persisted searchform textbox value in session storage. write logic in useSearchStr hook

--- web/box-office/src/components/SearchForm.jsx
+++ web/box-office/src/components/SearchForm.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
+import { useSearchStr } from '../lib/useSearchStr';
 
 const SearchForm = ({ onSearch }) => {
+  const [searchStr, setSearchStr] = useSearchStr();
   const [searchOption, setSearchOption] = useState('shows');
-  const [searchStr, setSearchStr] = useState('');
 
   const onSearchInputChange = ev => {
     setSearchStr(ev.target.value);
