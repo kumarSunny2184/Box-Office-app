ee7ffdb6fd5abeaf2bb01bbc8bcc487e509237d8
sunny kumar <kumarsunnysid343@gmail.com>
2023-07-28 10:19:11 +0530

    added fade-in animation to flexGrid

--- web/box-office/src/components/shows/ShowCard.jsx
+++ web/box-office/src/components/shows/ShowCard.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { styled } from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../Common/SearchCard';
 import { StarIcon } from '../Common/StarIcon';
@@ -6,6 +7,20 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '...'
     : 'No description';
+
+  const starBtnRef = useRef();
+
+  const handleStarClick = () => {
+    onStarMeClick(id);
+
+    const starBtnEl = starBtnRef.current;
+
+    if (!starBtnEl) {
+      starBtnEl.classList.remove('animate');
+    } else {
+      starBtnEl.classList.add('animate');
+    }
+  };
 
   return (
     <SearchCard>
@@ -21,7 +36,7 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read more
         </a>
-        <StarBtn type="button" onClick={() => onStarMeClick(id)}>
+        <StarBtn ref={starBtnRef} type="button" onClick={handleStarClick}>
           <StarIcon active={isStarred} />
         </StarBtn>
       </ActionSection>
@@ -57,5 +72,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
